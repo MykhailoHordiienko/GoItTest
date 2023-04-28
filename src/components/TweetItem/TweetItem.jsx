f55a88040b537cf726240890cdcd807981f91f78
Mykhailo Hordiienko <gordienkomichael191@gmail.com>
2023-04-28 13:12:04 +0300

    decoration elements

--- src/components/TweetItem/TweetItem.jsx
+++ src/components/TweetItem/TweetItem.jsx
@@ -5,13 +5,17 @@ export const TweetItem = () => {
     <li>
       <article className="w-[380px] h-[460px] rounded-[20px] bg-mainGradient">
         <div className="h-full bg-bgLogo bg-no-repeat bg-[left_20px_top_20px]">
-          <div className="flex flex-col items-center justify-end h-full bg-bgTweet bg bg-no-repeat bg-[center_top_28px]">
-            <div className="mb-[26px] rounded-[100%] border-[9px] border-mainWight shadow-avatarBoxShadow before:border-[9px] before:absolute before:w-[380px]">
-              <img
-                className="w-[62px] h-[62px]"
-                src={user}
-                alt="user"
-              />
+          <div className="flex flex-col items-center justify-end h-full bg-bgTweet bg-no-repeat bg-[center_top_28px]">
+            <div className="relative before:h-[9px] before:w-[380px] before:absolute before:bg-mainWight z-0 before:z-[-1] before:shadow-avatarLineShadow before:inset-y-1/2 before:inset-x-1/2 before:-translate-y-[150%] before:-translate-x-1/2">
+              <div className="mb-[26px] flex justify-center items-center w-[80px] h-[80px] rounded-[50%] bg-mainWight shadow-avatarBoxShadow ">
+                <div className="flex justify-center items-center w-[62px] h-[62px] rounded-[50%] bg-mainGradient">
+                  <img
+                    className="w-[62px] inline-block"
+                    src={user}
+                    alt="user"
+                  />
+                </div>
+              </div>
             </div>
             <p className="mb-[16px]"> 777 tweets</p>
             <p className="mb-[26px]">100,501 Followers</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,7 @@ module.exports = {
         "linear-gradient(114.99deg, #471CA9 -0.99%, #5736A3 54.28%, #4B2A99 78.99%)",
       bgTweet: "url('/src/img/bg-tweet.png')",
       bgLogo: "url('/src/img/logo.png')",
+      avatarCircle: "url('/src/img/avatar-circle.svg')",
     },
     colors: {
       mainBlack: "#373737",
@@ -26,6 +27,8 @@ module.exports = {
       btnBoxShadow: "0px 3.43693px 3.43693px rgba(0, 0, 0, 0.25)",
       avatarBoxShadow:
         "0px 4.39163px 4.39163px rgba(0, 0, 0, 0.06), inset 0px -2.19582px 4.39163px #AE7BE3, inset 0px 4.39163px 3.29372px #FBF8FF",
+      avatarLineShadow:
+        "0px 3.43693px 3.43693px rgba(0, 0, 0, 0.06), inset 0px -1.71846px 3.43693px #AE7BE3, inset 0px 3.43693px 2.5777px #FBF8FF",
     },
   },
   plugins: [],
