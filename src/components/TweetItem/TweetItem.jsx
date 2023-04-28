2bed6d109c2153badfd0cc49c6b6bf2a4fe45a2d
Mykhailo Hordiienko <gordienkomichael191@gmail.com>
2023-04-28 11:19:12 +0300

    add bg tweets

--- src/components/TweetItem/TweetItem.jsx
+++ src/components/TweetItem/TweetItem.jsx
@@ -3,21 +3,25 @@ import user from "../../gif/Hansel.png";
 export const TweetItem = () => {
   return (
     <li>
-      <article className="w-[380px] h-[460px] flex flex-col items-center justify-end rounded-[20px] bg-mainGradient">
-        <div className="mb-[26px]">
-          <img
-            className="w-[62px] h-[62px]"
-            src={user}
-            alt="user"
-          />
+      <article className="w-[380px] h-[460px] rounded-[20px] bg-mainGradient">
+        <div className="h-full bg-bgLogo bg-no-repeat bg-[left_20px_top_20px]">
+          <div className="flex flex-col items-center justify-end h-full bg-bgTweet bg bg-no-repeat bg-[center_top_28px]">
+            <div className="mb-[26px] rounded-[100%] border-[9px] border-mainWight shadow-avatarBoxShadow before:border-[9px] before:absolute before:w-[380px]">
+              <img
+                className="w-[62px] h-[62px]"
+                src={user}
+                alt="user"
+              />
+            </div>
+            <p className="mb-[16px]"> 777 tweets</p>
+            <p className="mb-[26px]">100,501 Followers</p>
+            <button
+              className="mb-[36px]"
+              type="button">
+              Button
+            </button>
+          </div>
         </div>
-        <p className="mb-[16px]"> 777 tweets</p>
-        <p className="mb-[26px]">100,501 Followers</p>
-        <button
-          className="mb-[36px]"
-          type="button">
-          Button
-        </button>
       </article>
     </li>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,8 @@ module.exports = {
     backgroundImage: {
       mainGradient:
         "linear-gradient(114.99deg, #471CA9 -0.99%, #5736A3 54.28%, #4B2A99 78.99%)",
+      bgTweet: "url('/src/img/bg-tweet.png')",
+      bgLogo: "url('/src/img/logo.png')",
     },
     colors: {
       mainBlack: "#373737",
@@ -19,6 +21,11 @@ module.exports = {
       mainBg: "#766A92",
       mainBtnColor: "#EBD8FF",
       accentBtnColor: "#5CD3A8",
+    },
+    boxShadow: {
+      btnBoxShadow: "0px 3.43693px 3.43693px rgba(0, 0, 0, 0.25)",
+      avatarBoxShadow:
+        "0px 4.39163px 4.39163px rgba(0, 0, 0, 0.06), inset 0px -2.19582px 4.39163px #AE7BE3, inset 0px 4.39163px 3.29372px #FBF8FF",
     },
   },
   plugins: [],
